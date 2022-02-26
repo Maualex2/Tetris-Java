271b23105b68f443978ca94a1c3f4438bbc57c22
Alexandre Maurice <alexandre.maurice03@gmail.com>
2022-02-26 16:39:07 +0100

    g

---
+++ Fenetre.java
@@ -0,0 +1,5 @@
+public class Fenetre{
+    public static void main(String[] args) {
+        
+    }
+}
--- IHM.java
+++
@@ -1,5 +0,0 @@
-public class IHM{
-    public static void main(String[] args) {
-        
-    }
-}
