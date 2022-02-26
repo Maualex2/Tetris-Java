fe13f280afeaec7d5768c8ddacd859d27e5ef536
Alexandre Maurice <alexandre.maurice03@gmail.com>
2022-02-26 17:12:29 +0100

    Interface Initiale

--- Fenetre.java
+++ Fenetre.java
@@ -1,9 +1,36 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Color;
 public class Fenetre extends JFrame{
+    JPanel Terrain;
+    JPanel Interface;
+    JPanel Page;
     public Fenetre(){
         super("Tetris");
-        setSize(1200,800);
+        setSize(800,900);
         setLocation(460, 140);
+        //Terrain de jeu
+        Terrain= new JPanel();
+        Terrain.setLayout(null);
+        Terrain.setBounds(20,50,400,800);
+        Terrain.setBackground(Color.black);
+
+        //Interface
+        Interface= new JPanel();
+        Interface.setLayout(null);
+        Interface.setBounds(440,50,300,800);
+        Interface.setBackground(Color.red);
+
+        //Fenetre entière
+        Page= new JPanel();
+        Page.setLayout(null);
+        Page.setBounds(0,0,800,900);
+        Page.setBackground(Color.yellow);
+        Page.add(Terrain);
+        Page.add(Interface);
+        this.add(Page);
+
+
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        // Pour l'instant j'ai juste setup l'interface graphique pour tester un peu le git hub
         Fenetre f = new Fenetre();
     }
 }
