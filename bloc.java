68d193334fe22fe58d42416bf1280c782d17f157
Alexandre Maurice <alexandre.maurice03@gmail.com>
2022-03-03 09:49:36 +0100

    Suite

---
+++ bloc.java
@@ -0,0 +1,10 @@
+import java.awt.Color;
+public class bloc {
+    String forme;
+    Color couleur;
+    int vide;
+
+    public bloc(String forme){
+        this.forme=forme;
+    }
+}
---
+++ limiteterrain.java
@@ -0,0 +1,9 @@
+import java.awt.Color;
+public class limiteterrain extends bloc{
+    
+    public limiteterrain(){
+        super("Limite");
+        couleur= Color.BLACK;
+    }
+
+}
