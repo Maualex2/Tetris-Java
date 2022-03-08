e6226879bed433e4a754d1881c1d8541df2394e1
Alexandre Maurice <alexandre.maurice03@gmail.com>
2022-03-08 22:28:07 +0100

    Méthodes Terrain de jeu

--- TerrainDeJeu.java
+++ TerrainDeJeu.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.awt.Color;
 
 public class TerrainDeJeu {
     public Bloc[][] Terrain;
@@ -12,11 +13,97 @@ public class TerrainDeJeu {
         Terrain = new Bloc[21][13];
     }
 
-    public void LigneComplete(){
-        //à mon avis il faut aussi faire appel à une méthode qui décale tout les blocs se serait plus beau à coder
+    public void LigneComplete(){ // parcourt le tableau et compte les blocs
+        int bonus = 0;
+        for (int i = 0; i < Terrain.length; i++) {
+            int nonvide = 0;
+            for (int j = 0; j < Terrain[i].length; j++) {
+                if (Terrain[i][j].vide == false) {
+                    nonvide++;
+                }
+            }
+            if (nonvide==13){
+                DecaleEnBas(i);
+                bonus++; // j'ai juste fait un bonus pour valoriser les enchainements
+                points=points+13*bonus;
+            }
+        }
     }
-    public void DecaleEnBas(int ligne){
+
+    public void DecaleEnBas(int ligne){ // c'est une méthode qui décale les blocs qui ne sont pas fixes vers le bas à partir d'une ligne que l'on précise
+        for (int i = ligne; i > 0 ; i--) { //ligne parcourt du bas du tableau jusqu'en haut
+            for (int j = 0; j < Terrain[i].length; j++) { // Colonne
+                if(!Terrain[i][j].dernierbloc){
+                    Terrain[i-1][j]=Terrain[i][j];
+                    Terrain[i][j]= new Bloc(Color.BLACK, false, true);
+                }
+            }
+        }
+    }
+    public void GagnePerdu(){ //Définir la notion de Perdu et de gagner
 
     }
+    public void ajouterForme(){
+
+    }
+    public void bougerDroite(){ //@Maualex2 c'est pas très optimisé car je fais 2 tour du tableau
+        boolean possible = true;
+        for (int i = 0; i < Terrain[12].length; i++) { // @Maualex2 teste si la forme est sur le coté droit
+            if (Terrain[i][12].dernierbloc==true) {
+                possible=false;
+                return;
+            }
+        }
+        for (int i = 0; i < Terrain.length; i++) { // @Maualex2 on verifie que le coté droit est bien vide ou rempli d'un des blocs de la forme
+            for (int j = 0; j < Terrain[i].length; j++) {
+                if(Terrain[i][j].dernierbloc==true && (!Terrain[i][j+1].vide==true || !Terrain[i][j+1].dernierbloc==true)){
+                    possible=false; // @Maualex2 On peut mettre un return pour que ce soit plus rapide
+                }
+            }
+        }
+        if(possible==true){// ensuite on fait le déplacement;
+            for (int i = 0; i < Terrain.length; i++) { // on verifie que le coté droit est bien vide ou rempli d'un des blocs de la forme
+                for (int j = 0; j < Terrain[i].length; j++) {
+                    if(Terrain[i][j].dernierbloc==true ){
+                    Terrain[i][j+1]=Terrain[i][j];
+                    Terrain[i][j]=new Bloc(Color.black,false,true);
+                    }
+                }
+            }
+        }
+    }
+    public void bougerGauche(){
+        boolean possible = true;
+        for (int i = 0; i < Terrain[0].length; i++) { // @Maualex2 teste si la forme est sur le coté droit
+            if (Terrain[i][12].dernierbloc==true) {
+                possible=false;
+                return;
+            }
+        }
+        for (int i = 0; i < Terrain.length; i++) { // @Maualex2 on verifie que le coté droit est bien vide ou rempli d'un des blocs de la forme
+            for (int j = 0; j < Terrain[i].length; j++) {
+                if(Terrain[i][j].dernierbloc==true && (!Terrain[i][j-1].vide==true || !Terrain[i][j-1].dernierbloc==true)){
+                    possible=false; // @Maualex2 On peut mettre un return pour que ce soit plus rapide
+                }
+            }
+        }
+        if(possible==true){// ensuite on fait le déplacement;
+            for (int i = 0; i < Terrain.length; i++) { // on verifie que le coté droit est bien vide ou rempli d'un des blocs de la forme
+                for (int j = 0; j < Terrain[i].length; j++) {
+                    if(Terrain[i][j].dernierbloc==true ){
+                    Terrain[i][j-1]=Terrain[i][j];
+                    Terrain[i][j]=new Bloc(Color.black,false,true);
+                    }
+                }
+            }
+        }
+    }
+    public void descendre(){
+      // TODO document why this method is empty
+    }
+    public void tourner(){
+      // TODO document why this method is empty
+    }
+
 
 }
--- bloc.java
+++ bloc.java
@@ -2,9 +2,11 @@ import java.awt.Color;
 public class Bloc {
     Color couleur;
     boolean dernierbloc;
+    boolean vide;
 
-    public Bloc(Color c, boolean d){
+    public Bloc(Color c, boolean d, boolean v){
         couleur=c;
         dernierbloc= d;
+        vide=false;
     }
 }
