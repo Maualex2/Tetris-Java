1a568b7f173baa390190ab3e0466029c5b0298d4
Maualex2 <alexandre.maurice03@gmail.com>
2022-03-19 11:30:29 +0100

    m

--- GraphiqueListedAttente.java
+++ GraphiqueListedAttente.java
@@ -8,20 +8,20 @@ public class GraphiqueListedAttente extends JPanel{
     }
     public void paint(Graphics g){ // les commentaires sont les formes finales, la c'est juste un test pour voir l'affichage
         g.setColor(Color.black);
-        g.fillRect(50,50,700,700);
+        g.fillRect(0,0,300,800);
 
         //Jeu.FormeEnAttente.Coordonnees[][] 
          int [][]tab={{0,0},{1,0},{0,1},{1,1}};
          for (int i = 0; i < tab.length; i++) {
             g.setColor(Color.blue);
             //g.setColor(Jeu.FormeEnAttente.color);
-            g.fillRect(150+(tab[i][0])*50, 150+(tab[i][1])*50, 50, 50);
+            g.fillRect(100+(tab[i][0])*50, 100+(tab[i][1])*50, 50, 50);
         } 
 
         g.setColor(Color.red);
         for (int i = 0; i < 6; i++) {
-            g.drawLine(50+50*i,50,50+50*i,300);
-            g.drawLine(50,50+50*i,350,50+50*i);
+            g.drawLine(50+50*i,0,50+50*i,300);
+            g.drawLine(0,50+50*i,350,50+50*i);
         } 
 
         
