c857d691ed533d5d8f8c421d6366376ebb0d3faf
Cobranyan <yann.tobet.clb.2016@gmail.com>
2022-03-20 15:11:17 +0100

    o

--- Main.java
+++ Main.java
@@ -4,25 +4,25 @@ public class Main {
         // création des formes utilisées
         double []origine={0,0};
         double [][]tab={{-1,1},{0,1},{1,1},{0,0}};
-        forme Tsimo= new forme(tab, origine,Color.RED,"t");
+        forme Tsimo= new forme(tab, origine,Color.RED);
 
         double [][]tabO={{-0.5,-0.5},{0.5,-0.5},{0.5,0.5},{0.5,-0.5}};
-        forme Osimo= new forme(tabO, origine,Color.yellow,"o");
+        forme Osimo= new forme(tabO, origine,Color.yellow);
 
         double [][]tabL={{-2,0},{-1,0},{0,0},{0,1}};
-        forme Lsimo= new forme(tabL, origine,Color.cyan,"l");
+        forme Lsimo= new forme(tabL, origine,Color.cyan);
 
         double [][]tabJ={{2,0},{1,0},{0,0},{0,1}};
-        forme Jsimo= new forme(tabJ, origine,Color.blue,"j");
+        forme Jsimo= new forme(tabJ, origine,Color.blue);
 
         double [][]tabI={{-1.5,0},{-0.5,0},{0.5,0},{1.5,0}};
-        forme Isimo= new forme(tabI, origine,Color.orange,"i");
+        forme Isimo= new forme(tabI, origine,Color.orange);
 
         double [][]tabS={{0,0.5},{1,0.5},{0,-0.5},{-1,-0.5}};
-        forme Ssimo= new forme(tabS, origine,Color.green,"s");
+        forme Ssimo= new forme(tabS, origine,Color.green);
         
         double [][]tabZ={{-1,0.5},{0,0.5},{0,-0.5},{1,-0.5}};
-        forme Zsimo= new forme(tabZ, origine,Color.magenta,"z");
+        forme Zsimo= new forme(tabZ, origine,Color.magenta);
         // tableau de forme utilisés pour le choix aléatoire
         forme [] figure= {Tsimo,Osimo,Lsimo,Jsimo, Isimo, Ssimo,Zsimo};
 
--- forme.java
+++ forme.java
@@ -4,13 +4,11 @@ public class forme {
     public double [][]Coordonees;
     public double []origine;
     public Color couleur;
-    public String type;
 
-    public forme(double [][] tab, double []dim, Color c, String a){
+    public forme(double [][] tab, double []dim, Color c){
         couleur= c;
         Coordonees=tab;
         origine=dim;
-        type=a;
 
     }
 
