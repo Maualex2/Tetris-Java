4df1a80ad15bf27d385ff1cc9d84388ef3e3a024
Cobranyan <yann.tobet.clb.2016@gmail.com>
2022-04-11 21:20:34 +0200

    yo

--- MesReglesDuJeu.java
+++ MesReglesDuJeu.java
@@ -3,12 +3,31 @@ import java.awt.*;
 
 public class MesReglesDuJeu extends JFrame{
 
+    JPanel conteneur;
+    JLabel logo;
+    JTextField Objectif;
+    JLabel LesTouches;
+    JTextField lesTouches;
+    JLabel LigneComp;
+    JTextField complete;
+    JLabel FinduJeu;
+    JTextField perdre;
+    JTextField battezVotreScore;
 
     public MesReglesDuJeu (){
         super("Règle du Tetrinsa");
         setSize(500,600);
         setLocation(1000, 100);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE) ;
+
+        JScrollPane scroll= new JScrollPane(conteneur);
+        scroll.setBounds(0,0,320,150);
+
+        JPanel conteneur = new JPanel();
+		conteneur.setLayout(null);
+		conteneur.setBounds(0,0,500,900) ;
+		conteneur.setBackground(Color.black);
+        add(conteneur);
 		
 		setVisible(false);
     }
