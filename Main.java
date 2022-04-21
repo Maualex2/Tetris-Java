c1f5a9bbd91bacae6f9b0f89650bb7980bd2a754
Maualex2 <alexandre.maurice03@gmail.com>
2022-04-21 16:36:18 +0200

    g

--- Main.java
+++ Main.java
@@ -29,6 +29,7 @@ public class Main {
         
         TerrainDeJeu Jeu = new TerrainDeJeu(figure);
         Interface f = new Interface(Jeu);
+        String [] parts = "19/01/2018".split( "/" );
     }
         
 }
